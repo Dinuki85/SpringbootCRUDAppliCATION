2366051ea834ea5685fd7aa724748842ce7172ad
Dinuki85 <prarthanadinuki2@gmail.com>
2025-08-17 18:36:17 +0530

    Added navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-
+import "../node_modules/bootstrap/dist/css/bootstrap-grid.min.css"
 import './App.css';
+import { Navbar } from "./layout/Navbar";
 
 function App() {
   return (
-    <>
+    <div className="App">
+      <Navbar/>
+
       <h1 className='text-center'>Create CRUD application</h1>
-    </>
+    </div>
   );
 }
 

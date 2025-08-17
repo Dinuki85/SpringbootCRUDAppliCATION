ef69027ed3c49403d91ba87761784925866e1690
Dinuki85 <prarthanadinuki2@gmail.com>
2025-08-17 18:59:58 +0530

    Imported the home page for view the users

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import './App.css';
 import { Navbar } from "./layout/Navbar";
+import Home from "./pages/Home";
 
 function App() {
   return (
@@ -8,7 +9,9 @@ function App() {
       {/* Import the navigation bar*/}
       <Navbar/>
 
-      <h1 className='text-center'>Create CRUD application</h1>
+      <h1 className='text-center bold'>VIEW USERS FROM HERE</h1>
+      {/*Adding Home page to see the users */}
+      <Home/>
     </div>
   );
 }

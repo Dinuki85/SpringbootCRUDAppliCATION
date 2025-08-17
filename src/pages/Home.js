a46118f2c4fc44825a7ce60254d458792fa1dc18
Dinuki85 <prarthanadinuki2@gmail.com>
2025-08-18 01:27:46 +0530

    Added background image to the main

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,17 +17,38 @@ export default function Home() {
   };
 
   //For deleting the users
-  const deleteUser = async (id)=>{
-    await axios.delete(`http://localhost:8080/api/user/${id}`)
+  const deleteUser = async (id) => {
+    await axios.delete(`http://localhost:8080/api/user/${id}`);
     loadUsers();
-  }
+  };
+
+  // Background style
+  const backgroundStyle = {
+    backgroundImage: "url('/assets/background.jpg')", // replace with your image path
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+    width: "100vw",
+    height: "100vh",
+    position: "fixed",
+    top: 0,
+    left: 0,
+    zIndex: -1,
+  };
+
   return (
-    <div className="container ">
-      <div className="py-5">
-          <h1 className='text-center bold'>VIEW USERS FROM HERE</h1>
-        {/*Adding Bootstrap table */}
-        <table className="table border shadow table-hover">
-          <thead>
+    <div>
+      {/* Full screen background */}
+      <div style={backgroundStyle}></div>
+
+      {/* Main content */}
+      <div className="container py-5" style={{ position: "relative", zIndex: 1 }}>
+        <h1 className="mb-4 text-center text-white bold">
+          VIEW USERS FROM HERE
+        </h1>
+        {/*Interactive Bootstrap table with hover and striped rows */}
+        <table className="table shadow table-hover table-striped table-bordered bg-light">
+          <thead className="table-dark">
             <tr>
               <th scope="col">Id</th>
               <th scope="col">Full Name</th>
@@ -38,24 +59,34 @@ export default function Home() {
           </thead>
           <tbody>
             {/*Map the details that need to displayed in the table */}
-            {users.map((user,index) =>(
-                 <tr  key={index}>
-              <th scope="row">{index+1}</th>
-              <td>{user.name}</td>
-              <td>{user.username}</td>
-              <td>{user.email}</td>
-              <td>
-                <Link className="mx-2 btn btn-outline-primary" to={`/viewuser/${user.id}`}><strong>View</strong></Link>
-                <Link className="mx-2 btn btn-primary" to={`/edituser/${user.id}`}>
-                Update
-                </Link>
-                <button className="mx-2 btn btn-danger" onClick={()=>deleteUser(user.id)}>Delete</button>
-
-              </td>
-            </tr>
-            ))
-        }
-            
+            {users.map((user, index) => (
+              <tr key={index} className="align-middle">
+                <th scope="row">{index + 1}</th>
+                <td>{user.name}</td>
+                <td>{user.username}</td>
+                <td>{user.email}</td>
+                <td>
+                  <Link
+                    className="mx-2 btn btn-outline-primary"
+                    to={`/viewuser/${user.id}`}
+                  >
+                    <strong>View</strong>
+                  </Link>
+                  <Link
+                    className="mx-2 btn btn-primary"
+                    to={`/edituser/${user.id}`}
+                  >
+                    Update
+                  </Link>
+                  <button
+                    className="mx-2 btn btn-danger"
+                    onClick={() => deleteUser(user.id)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>

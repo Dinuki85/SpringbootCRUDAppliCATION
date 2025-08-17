65bcf1cfeb689540e3097213b177cf2f4025ab65
Dinuki85 <prarthanadinuki2@gmail.com>
2025-08-17 23:48:13 +0530

    Added method for editing the user with error message if the user doesn't exists

--- src/main/java/com/example/CRUDApplication/controller/UserController.java
+++ src/main/java/com/example/CRUDApplication/controller/UserController.java
@@ -35,4 +35,15 @@ public class UserController {
 
     }
 
+    @PutMapping("/{id}")
+    User updateUser(@RequestBody User newUser,@PathVariable Long id){
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setUsername(newUser.getUsername());
+                    user.setName(newUser.getName());
+                    user.setEmail(newUser.getEmail());
+                    return userRepository.save(user);
+                }).orElseThrow(()->new UserNotFoundException(id));
+    }
+
 }
